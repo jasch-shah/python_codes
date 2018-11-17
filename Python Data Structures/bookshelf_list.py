7148330e4cfb49c121571f332cf5e6ce42dc2b8d
Jash Shah <jash.shah20@gmail.com>
2018-11-17 06:59:40 +0530

    Add files via upload

---
+++ Python Data Structures/bookshelf_list.py
@@ -0,0 +1,8 @@
+bookshelf = []
+bookshelf.append("The Hard Thing about Hard Things")
+bookshelf.append("Shoe Dog")
+bookshelf.append("A Monk who Sold his Ferrari")
+bookshelf.append("The Subtle Art of Not Giving a F**k")
+
+for i in range(0,4):
+	print(bookshelf[i])
